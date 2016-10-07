0b3863af201d8af65c8a108490faff6a16cd48c6
ingridh <ingridhong@gmail.com>
2016-10-07 02:04:02 +0100

    skeleton of a site

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$( document ).ready(function() {
+	$(window).resize(function() {
+        console.log(window.innerHeight);
+        document.getElementById(('work')).style.marginBottom = String(window.innerHeight-350) +'px';
+    }).resize();
+
+	var titles = [
+    'Azimo',
+    'MongoDB',
+    'frog Design',
+    'a drone app',
+    'IoT with Samsung',
+    'the NextDoor app',
+    'digital devices',
+    'a laptop table',
+    'keep designing'
+	];
+
+	var subtitle = [
+	"I'm currently working at",
+	"I worked at",
+	"I designed",
+	"I redesigned",
+	"I explored",
+	"I built",
+	"I would love to"
+	];
+
+	$(window).scroll(function(){
+	    var pos = $(this).scrollTop();
+	    //console.log('azimo ' + ($("#azimo").offset().top - $(window).scrollTop()));
+		//console.log('mongo ' + ($("#mongo").offset().top - $(window).scrollTop()));
+
+		if (($("#azimo").offset().top - $(window).scrollTop() < 100) && ($("#azimo").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[0]);
+			$('.werk').text(titles[0]);
+		}
+		if  (($("#mongo").offset().top - $(window).scrollTop() < 100) && ($("#mongo").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[1]);
+			$('.werk').text(titles[1]);
+		} 
+		if  (($("#frog").offset().top - $(window).scrollTop() < 100) && ($("#frog").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[1]);
+			$('.werk').text(titles[2]);
+		}
+		if  (($("#cs160").offset().top - $(window).scrollTop() < 100) && ($("#cs160").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[2]);
+			$('.werk').text(titles[3]);
+		}
+		if  (($("#iot").offset().top - $(window).scrollTop() < 100) && ($("#iot").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[4]);
+			$('.werk').text(titles[4]);
+		}
+		if  (($("#nextdoor").offset().top - $(window).scrollTop() < 100) && ($("#nextdoor").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[3]);
+			$('.werk').text(titles[5]);
+		}
+		if  (($("#cmake").offset().top - $(window).scrollTop() < 100) && ($("#cmake").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[5]);
+			$('.werk').text(titles[6]);
+		}
+		if  (($("#lapshelf").offset().top - $(window).scrollTop() < 100) && ($("#lapshelf").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[5]);
+			$('.werk').text(titles[7]);
+		}
+		if  (($("#contact").offset().top - $(window).scrollTop() < 100) && ($("#contact").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').text(subtitle[6]);
+			$('.werk').text(titles[8]);
+		} 
+	});
+
+});
