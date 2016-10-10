6c4aaf9d592a4b61d6061bfe34e4b25997f94f49
ingridh <ingridhong@gmail.com>
2016-10-10 01:59:22 +0100

    fixed scorll issue past the last box, also listening to book of mormon as i do this

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@ $( document ).ready(function() {
     'IoT with Samsung',
     'the NextDoor app',
     'digital devices',
-    'keep designing'
+    'keep designing',
+    ''
 	];
 
 	var subtitle = [
@@ -21,43 +22,47 @@ $( document ).ready(function() {
 	"I redesigned",
 	"I explored",
 	"I built",
-	"I would love to"
+	"I would love to",
+	""
 	];
 
 	$(window).scroll(function(){
 	    var pos = $(this).scrollTop();
-		if (($("#azimo").offset().top - $(window).scrollTop() < 100) && ($("#azimo").offset().top - $(window).scrollTop() > 0)) {
+		if (($("#azimo").offset().top - $(window).scrollTop() < 150) && ($("#azimo").offset().top - $(window).scrollTop() > 0)) {
 			$('.smalltitle').text(subtitle[0]);
 			$('.werk').text(titles[0]);
 		}
-		if  (($("#mongo").offset().top - $(window).scrollTop() < 100) && ($("#mongo").offset().top - $(window).scrollTop() > 0)) {
+		if  (($("#mongo").offset().top - $(window).scrollTop() < 150) && ($("#mongo").offset().top - $(window).scrollTop() > 0)) {
 			$('.smalltitle').text(subtitle[1]);
 			$('.werk').text(titles[1]);
 		} 
-		if  (($("#frog").offset().top - $(window).scrollTop() < 100) && ($("#frog").offset().top - $(window).scrollTop() > 0)) {
+		if  (($("#frog").offset().top - $(window).scrollTop() < 150) && ($("#frog").offset().top - $(window).scrollTop() > 0)) {
 			$('.smalltitle').text(subtitle[1]);
 			$('.werk').text(titles[2]);
 		}
-		if  (($("#cs160").offset().top - $(window).scrollTop() < 100) && ($("#cs160").offset().top - $(window).scrollTop() > 0)) {
+		if  (($("#cs160").offset().top - $(window).scrollTop() < 150) && ($("#cs160").offset().top - $(window).scrollTop() > 0)) {
 			$('.smalltitle').text(subtitle[2]);
 			$('.werk').text(titles[3]);
 		}
-		if  (($("#iot").offset().top - $(window).scrollTop() < 100) && ($("#iot").offset().top - $(window).scrollTop() > 0)) {
+		if  (($("#iot").offset().top - $(window).scrollTop() < 150) && ($("#iot").offset().top - $(window).scrollTop() > 0)) {
 			$('.smalltitle').text(subtitle[4]);
 			$('.werk').text(titles[4]);
 		}
-		if  (($("#nextdoor").offset().top - $(window).scrollTop() < 100) && ($("#nextdoor").offset().top - $(window).scrollTop() > 0)) {
+		if  (($("#nextdoor").offset().top - $(window).scrollTop() < 150) && ($("#nextdoor").offset().top - $(window).scrollTop() > 0)) {
 			$('.smalltitle').text(subtitle[3]);
 			$('.werk').text(titles[5]);
 		}
-		// if  (($("#cmake").offset().top - $(window).scrollTop() < 100) && ($("#cmake").offset().top - $(window).scrollTop() > 0)) {
-		// 	$('.smalltitle').text(subtitle[5]);
-		// 	$('.werk').text(titles[6]);
-		// }
-		if  (($("#contact").offset().top - $(window).scrollTop() < 100) && ($("#contact").offset().top - $(window).scrollTop() > 0)) {
+		if  (($("#contact").offset().top - $(window).scrollTop() < 150) && ($("#contact").offset().top - $(window).scrollTop() > 0)) {
 			$('.smalltitle').text(subtitle[6]);
 			$('.werk').text(titles[7]);
-		} 
+		}  
+		if  (($("#links").offset().top - $(window).scrollTop() < 250) && ($("#links").offset().top - $(window).scrollTop() > 0)) {
+			$('.smalltitle').hide();
+			$('.werk').hide();
+		} else {
+			$('.smalltitle').show();
+			$('.werk').show();
+		}
 	});
 
 });
